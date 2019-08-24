20d264d5bcdae5c35d2ff93936a531699a5239c4
Fredrick Barrett <f.barrett12@gmail.com>
2019-08-24 07:37:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,6 +30,5 @@ def find_the_cheese(cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_types.find do |cheese|
-  cheese_types.include?(cheese)
   end 
 end
